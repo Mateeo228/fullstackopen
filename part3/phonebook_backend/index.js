c9685c96d27f79aee3243746c584ba5a103e2a6d
Mateo Lazarte <mateolazarte07@gmail.com>
2023-01-30 11:11:45 -0300

    Exercises 3.19, 3.20, 3.21

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -35,7 +35,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
     next(error)
 }
@@ -77,20 +79,8 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if (!body.name) {
-        return response.status(400).json({ 
-        error: 'name missing' 
-        })
-    }
-
-    if (!body.number) {
-        return response.status(400).json({ 
-        error: 'number missing' 
-        })
-    }
 
     const person = new Person({
         name: body.name,
@@ -100,6 +90,7 @@ app.post('/api/persons', (request, response) => {
     person
         .save()
         .then(savedPerson => response.json(savedPerson))
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
